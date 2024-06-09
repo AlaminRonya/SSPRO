e89f98106037f099baca71bc16fe0bde8811789a
AlaminRonya <hprony12@gmail.com>
2024-06-09 23:54:20 +0600

    Git Test a1

---
+++ TestDemo/src/test/java/com/alamin/testdemo/TestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alamin.testdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
