6cc3a3401cabfd986adf3140047551e3115f1443
AlaminRonya <hprony12@gmail.com>
2024-06-29 14:24:43 +0600

    cbs init

---
+++ nodeProject/app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const __dirname = path.resolve();
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
