b1560bab1bc86a219e3fb4d7729250d6c31c6558
AlaminRonya <hprony12@gmail.com>
2024-07-05 23:06:30 +0600

    init interceptor

---
+++ InterceptorDemo/src/test/java/com/alamin/InterceptorDemo/InterceptorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alamin.InterceptorDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterceptorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
