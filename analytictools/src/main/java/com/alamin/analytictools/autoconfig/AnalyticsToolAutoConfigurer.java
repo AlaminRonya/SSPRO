049a773426824680cd01af178eed252411447dc0
AlaminRonya <hprony12@gmail.com>
2024-07-08 22:33:31 +0600

    created maven dependency for local and add to Condition configuration

--- analytictools/src/main/java/com/alamin/analytictools/autoconfig/AnalyticsToolAutoConfigurer.java
+++ analytictools/src/main/java/com/alamin/analytictools/autoconfig/AnalyticsToolAutoConfigurer.java
@@ -1,9 +1,11 @@
 package com.alamin.analytictools.autoconfig;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnProperty(value = "com.alamin.analytictools.enable", havingValue = "true")
 @ComponentScan(basePackages = "com.alamin.analytictools")
 public class AnalyticsToolAutoConfigurer {
 }
