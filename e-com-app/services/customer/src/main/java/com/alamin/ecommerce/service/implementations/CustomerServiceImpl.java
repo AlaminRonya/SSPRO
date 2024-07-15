817196822d6d3586137ec41de259cf71b4b0dd06
AlaminRonya <hprony12@gmail.com>
2024-07-15 21:45:34 +0600

    Start Microservice customer-1

--- e-com-app/services/customer/src/main/java/com/alamin/ecommerce/service/implementations/CustomerServiceImpl.java
+++ e-com-app/services/customer/src/main/java/com/alamin/ecommerce/service/implementations/CustomerServiceImpl.java
@@ -1,4 +1,12 @@
 package com.alamin.ecommerce.service.implementations;
 
-public class CustomerServiceImpl {
+import com.alamin.ecommerce.service.interfaces.CustomerService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class CustomerServiceImpl implements CustomerService {
 }
